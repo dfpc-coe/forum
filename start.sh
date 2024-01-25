4de2a2b407b005e1b167148f5bd092e050139e4e
ingalls <nick@ingalls.ca>
2024-01-24 21:31:41 -0700

    Mv config

--- start.sh
+++ start.sh
@@ -1,9 +1,12 @@
+mkdir -p $HOME/config/
+
 while true
 do
     if test -f ${CONFIG_DIR}/config.json; then
         ./nodebb start --config=${CONFIG_DIR}/config.json || true
     else
         ./nodebb start || true
+        mv $HOME/NodeBB-${VERSION}/config.json ${CONFIG_DIR}/config.json
     fi
 
     echo "Restart Script"
